45fb91d513dbb0e7cc5a826d1e1d4a2eb6d49e18
ktnydi <kitano.prog@gmail.com>
2021-02-11 16:39:55 +0900

    Add: レポート詳細ページを作成

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:club_walk_mock/edit_profile_page.dart';
+import 'package:club_walk_mock/report_detail_page.dart';
 import 'package:club_walk_mock/setting_page.dart';
 import 'package:flutter/material.dart';
 
@@ -163,93 +164,106 @@ class Profile extends StatelessWidget {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(16),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 10,
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Material(
-                            elevation: 1,
-                            borderRadius: BorderRadius.circular(24),
-                            clipBehavior: Clip.antiAlias,
-                            child: Container(
-                              width: 48,
-                              height: 48,
-                              child: Image.network(
-                                'https://avatars.githubusercontent.com/u/36891892?s=460&u=37a046d0285781e133c0d4958536fe042a969b85&v=4',
+                    clipBehavior: Clip.antiAlias,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ReportDetail(),
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Material(
+                              elevation: 1,
+                              borderRadius: BorderRadius.circular(24),
+                              clipBehavior: Clip.antiAlias,
+                              child: Container(
                                 width: 48,
+                                height: 48,
+                                child: Image.network(
+                                  'https://avatars.githubusercontent.com/u/36891892?s=460&u=37a046d0285781e133c0d4958536fe042a969b85&v=4',
+                                  width: 48,
+                                ),
                               ),
                             ),
-                          ),
-                          SizedBox(width: 10),
-                          Flexible(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Flexible(
-                                  child: Text(
-                                    'John Smith',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16,
+                            SizedBox(width: 10),
+                            Flexible(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      'John Smith',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Text(
-                                  'I have walked on main street for 2 hours. I want to became slim.',
-                                ),
-                                index % 4 == 0
-                                    ? Padding(
-                                        padding: EdgeInsets.only(top: 10),
-                                        child: Material(
-                                          elevation: 1,
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                          clipBehavior: Clip.antiAlias,
-                                          child: Container(
-                                            width: double.infinity,
-                                            height: 160,
-                                            child: Image.network(
-                                              'https://images.unsplash.com/flagged/photo-1556746834-cbb4a38ee593?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
-                                              fit: BoxFit.cover,
+                                  Text(
+                                    'I have walked on main street for 2 hours. I want to became slim.',
+                                  ),
+                                  index % 4 == 0
+                                      ? Padding(
+                                          padding: EdgeInsets.only(top: 10),
+                                          child: Material(
+                                            elevation: 1,
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                            clipBehavior: Clip.antiAlias,
+                                            child: Container(
+                                              width: double.infinity,
+                                              height: 160,
+                                              child: Image.network(
+                                                'https://images.unsplash.com/flagged/photo-1556746834-cbb4a38ee593?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
+                                                fit: BoxFit.cover,
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                      )
-                                    : SizedBox(),
-                                SizedBox(height: 10),
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.favorite_border,
-                                      size: 16,
-                                    ),
-                                    SizedBox(width: 4),
-                                    Text(
-                                      '13',
-                                      style:
-                                          Theme.of(context).textTheme.subtitle2,
-                                    ),
-                                    SizedBox(width: 16),
-                                    Text(
-                                      'Reply',
-                                    ),
-                                    SizedBox(width: 4),
-                                    Text(
-                                      '4',
-                                      style:
-                                          Theme.of(context).textTheme.subtitle2,
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                        )
+                                      : SizedBox(),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.favorite_border,
+                                        size: 16,
+                                      ),
+                                      SizedBox(width: 4),
+                                      Text(
+                                        '13',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle2,
+                                      ),
+                                      SizedBox(width: 16),
+                                      Text(
+                                        'Reply',
+                                      ),
+                                      SizedBox(width: 4),
+                                      Text(
+                                        '4',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle2,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   );
---
+++ lib/report_detail_page.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+
+class ReportDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        clipBehavior: Clip.antiAlias,
+        label: Text(
+          'Reply Comment',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {},
+      ),
+      appBar: AppBar(
+        title: Text('Report'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  clipBehavior: Clip.antiAlias,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Flexible(
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Material(
+                                    elevation: 1,
+                                    shape: CircleBorder(),
+                                    clipBehavior: Clip.antiAlias,
+                                    child: Container(
+                                      width: 64,
+                                      height: 64,
+                                      child: Image.network(
+                                        'https://avatars.githubusercontent.com/u/36891892?s=460&u=37a046d0285781e133c0d4958536fe042a969b85&v=4',
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Flexible(
+                                    child: Text(
+                                      'John Smith',
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            OutlinedButton(
+                              child: Text('Follow'),
+                              style: OutlinedButton.styleFrom(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 8,
+                                ),
+                                minimumSize: Size(100, 0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          'I have walked on main street for 2 hours. I want to became slim.',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 16),
+                          child: Material(
+                            elevation: 1,
+                            borderRadius: BorderRadius.circular(8),
+                            clipBehavior: Clip.antiAlias,
+                            child: AspectRatio(
+                              aspectRatio: 5 / 3,
+                              child: Image.network(
+                                'https://images.unsplash.com/flagged/photo-1556746834-cbb4a38ee593?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.favorite_border,
+                              size: 18,
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              '13',
+                              style: Theme.of(context).textTheme.subtitle2,
+                            ),
+                            SizedBox(width: 16),
+                            Text(
+                              'Reply',
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              '4',
+                              style: Theme.of(context).textTheme.subtitle2,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ListView.separated(
+                padding: EdgeInsets.all(16),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 10,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Material(
+                            elevation: 1,
+                            borderRadius: BorderRadius.circular(24),
+                            clipBehavior: Clip.antiAlias,
+                            child: Container(
+                              width: 48,
+                              height: 48,
+                              child: Image.network(
+                                'https://avatars.githubusercontent.com/u/36891892?s=460&u=37a046d0285781e133c0d4958536fe042a969b85&v=4',
+                                width: 48,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Flexible(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Flexible(
+                                  child: Text(
+                                    'John Smith',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'I have walked on main street for 2 hours. I want to became slim.',
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.favorite_border,
+                                      size: 16,
+                                    ),
+                                    SizedBox(width: 4),
+                                    Text(
+                                      '13',
+                                      style:
+                                          Theme.of(context).textTheme.subtitle2,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(height: 8),
+                itemCount: 4,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
